91db19132cc886e6217e8a4bc1105dd90b4e1f39
Rodrigo Gonçalves <rodrigo.g.459@gmail.com>
2023-09-12 20:52:57 -0300

    WIP - Implementação de autenticação via JDBC

--- src/main/java/br/com/mvc/thymeleaf/marketFlow/config/WebSecurityConfig.java
+++ src/main/java/br/com/mvc/thymeleaf/marketFlow/config/WebSecurityConfig.java
@@ -1,14 +1,20 @@
 package br.com.mvc.thymeleaf.marketFlow.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 //import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,7 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
+	
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -25,6 +31,7 @@ public class WebSecurityConfig {
 				.and()
 				.formLogin(form -> form
 						.loginPage("/login")
+						.defaultSuccessUrl("/home", true)
 						.permitAll())
 						.logout(logout -> logout.logoutUrl("/logout"));
 				
@@ -32,14 +39,28 @@ public class WebSecurityConfig {
     }
     
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserDetails user = User.builder()
-            .username("user")
-            .password(encoder.encode("password"))
-            .roles("ADM")
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
             .build();
-        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+    	
+    	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    	
+    	UserDetails user = User.builder()
+    			 .username("user")
+    	            .password(encoder.encode("password"))
+    	            .roles("ADM")
+    	            .build();
+    	
+    	JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+    	users.createUser(user);
+    	return users;
     }
     
 }
+
