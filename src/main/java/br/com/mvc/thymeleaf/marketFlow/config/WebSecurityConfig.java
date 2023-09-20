5fba7610716f804f7f4488440eea6f70c5f2006e
Rodrigo Gonçalves <rodrigo.g.459@gmail.com>
2023-09-20 18:10:32 -0300

    Implementação de autenticação via JDBC

--- src/main/java/br/com/mvc/thymeleaf/marketFlow/config/WebSecurityConfig.java
+++ src/main/java/br/com/mvc/thymeleaf/marketFlow/config/WebSecurityConfig.java
@@ -2,22 +2,14 @@ package br.com.mvc.thymeleaf.marketFlow.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-//import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -38,29 +30,18 @@ public class WebSecurityConfig {
 		return http.build();
     }
     
-    @Bean
     public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
             .setType(EmbeddedDatabaseType.H2)
-            .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
             .build();
     }
 
-    @Bean
-    UserDetailsManager users(DataSource dataSource) {
-    	
-    	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    	
-    	UserDetails user = User.builder()
-    			 .username("user")
-    	            .password(encoder.encode("password"))
-    	            .roles("ADM")
-    	            .build();
-    	
-    	JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-    	users.createUser(user);
-    	return users;
-    }
     
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	
+        auth.jdbcAuthentication()
+        .withDefaultSchema()
+        .dataSource(dataSource());
+    }
 }
 
